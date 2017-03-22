cf136e072f4fa13fc9b827db6084fa630b9fdd41
Damien Picard <dam.pic@free.fr>
2017-03-22 11:47:17 +0100

    Class refactor.

--- morse_code_disktray.py
+++ morse_code_disktray.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 
-# morse code disktray, morse code communication through disk trays 
+# morse code disktray, morse code communication through disk trays
 # Copyright (C) 2017  Damien Picard dam.pic AT free.fr
 #
 # This program is free software: you can redistribute it and/or modify
@@ -21,12 +21,7 @@ import sys
 import subprocess
 import time
 
-TIME_UNIT = 0.2
-SHORT = TIME_UNIT * 1
-LONG = TIME_UNIT * 3
-# SHORT_GAP = TIME_UNIT * 3
-MEDIUM_GAP = TIME_UNIT * 7
-
+DBG = True
 MORSE_ALPHABET = {
     "A" : ".-",
     "B" : "-...",
@@ -78,65 +73,105 @@ MORSE_ALPHABET = {
     "=" : "-...-",
     }
 
-def tray_open():
-    # subprocess.Popen('eject')
-    print('OPEN')
+class Morse():
+    def __init__(self):
+        pass
 
-def tray_close():
-    # subprocess.Popen(['eject', '-t'])
-    print('CLOSE')
+    def set_time_unit(self, time_unit):
+        self.time_unit = time_unit
 
-def sleep(t, dbg=False):
-    time.sleep(t)
-    if dbg:
-        print(t)
+    def get_short_time(self):
+        return self.time_unit
+    def get_long_time(self):
+        return self.time_unit * 3
+    def get_word_gap(self):
+        return self.time_unit * 7
 
-DBG = True
+    def test_duration(self):
+        if DBG:
+            self.set_time_unit(0.2)
+            return
+        try:
+            start_time = time.time()
+            self.tray_open()
+            open_duration = time.time() - start_time
+        except:
+            self.set_time_unit(0.5)
+        try:
+            start_time = time.time()
+            self.tray_close()
+            close_duration = time.time() - start_time
+        except:
+            pass
+        self.set_time_unit(max(open_duration, close_duration) * 1.05)
 
-def intra_character():
-    sleep(SHORT, dbg=DBG)
+    def tray_open(self):
+        if not DBG:
+            sp = subprocess.Popen('eject')
+            sp.wait()
+        if DBG:
+            print('OPEN')
 
-def inter_letters():
-    sleep(LONG, dbg=DBG)
+    def tray_close(self):
+        if not DBG:
+            sp = subprocess.Popen(['eject', '-t'])
+            sp.wait()
+        if DBG:
+            print('CLOSE')
 
-def inter_words():
-    sleep(MEDIUM_GAP, dbg=DBG)
+    def sleep(self, t):
+        time.sleep(t)
+        if DBG:
+            print(t)
 
-def dit():
-    tray_open()
-    sleep(SHORT, dbg=DBG)
-    tray_close()
+    def intra_character(self):
+        self.sleep(self.get_short_time())
 
-def dah():
-    tray_open()
-    sleep(LONG, dbg=DBG)
-    tray_close()
+    def inter_letters(self):
+        self.sleep(self.get_long_time())
 
-def echo_char(c):
-    morse_rep = MORSE_ALPHABET[c]
-    for i, dd in enumerate(morse_rep):
-        if dd == '.':
-            dit()
-        elif dd == '-':
-            dah()
-        elif dd == '/':
-            inter_words()
-        if i < len(morse_rep)-1:
-            intra_character()
+    def inter_words(self):
+        self.sleep(self.get_word_gap())
 
-def echo_msg(msg):
-    for i, c in enumerate(msg):
-        print(c)
-        echo_char(c)
-        if i < len(msg)-1 and msg[i+1] != '/':
-            inter_letters()
+    def dit(self):
+        self.tray_open()
+        self.sleep(self.get_short_time())
+        self.tray_close()
+
+    def dah(self):
+        self.tray_open()
+        self.sleep(self.get_long_time())
+        self.tray_close()
+
+    def echo_char(self, c):
+        morse_rep = MORSE_ALPHABET[c]
+        for i, dd in enumerate(morse_rep):
+            if dd == '.':
+                self.dit()
+            elif dd == '-':
+                self.dah()
+            elif dd == '/':
+                self.inter_words()
+            if i < len(morse_rep)-1:
+                self.intra_character()
+
+    def echo_msg(self, msg):
+        for i, c in enumerate(msg):
+            print(c)
+            self.echo_char(c)
+            if i < len(msg)-1 and msg[i+1] != '/':
+                self.inter_letters()
 
 
 if __name__ == "__main__":
     args = sys.argv[1:]
+    msg = ''
     if args:
         msg = ' '.join(args)
-    else:
+    if not msg:
         msg = raw_input('Input message:')
     msg = msg.upper()
-    echo_msg(msg)
+
+    morse = Morse()
+    morse.test_duration()
+    morse.echo_msg(msg)
